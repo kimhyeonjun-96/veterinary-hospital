0da3f55061403c1e64daef7cdbcb0d73a5eb5798
김현준 <69852555+kimhyeonjun-96@users.noreply.github.com>
2024-01-20 22:58:02 +0900

    Resolve conflicts between createReservationLogic and main

--- src/test/java/com/myProject/treatment/domain/animal/service/AnimalServiceImplTest.java
+++ src/test/java/com/myProject/treatment/domain/animal/service/AnimalServiceImplTest.java
@@ -2,8 +2,10 @@ package com.myProject.treatment.domain.animal.service;
 
 import com.myProject.treatment.domain.animal.Animal;
 import com.myProject.treatment.domain.animal.dao.AnimalRepository;
+import com.myProject.treatment.domain.animal.dto.AnimalDTO;
 import com.myProject.treatment.domain.member.Member;
 import com.myProject.treatment.domain.member.dao.MemberRepository;
+import com.myProject.treatment.domain.member.dto.MemberDTO;
 import com.myProject.treatment.domain.member.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +26,13 @@ class AnimalServiceImplTest {
 
     @Test
     public void 회원_특정_반려동물(){
-//        Member memberDTO = memberService.findOneMember(14L).get();
+        MemberDTO memberDTO = memberService.findOneMember(14L);
 
-//        List<Animal> animalList = animalService.getAnimal(memberDTO.getId());
+        List<AnimalDTO> animalList = animalService.getAnimalList(memberDTO.getId());
 
-//        for(Animal animal : animalList){
-//            System.out.println(animal);
-//        }
-
+        for(AnimalDTO animal : animalList){
+            System.out.println(animal);
+        }
     }
 
 }
